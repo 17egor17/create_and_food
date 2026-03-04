6e3eab0f9bf2d960326233fd74d98c5b5566e52f
Егор <egornurkanov31@gmail.com>
2026-03-04 19:19:19 +0300

    Commit

--- src/main/java/net/egorplaytv/caf/util/CAFTags.java
+++ src/main/java/net/egorplaytv/caf/util/CAFTags.java
@@ -1,6 +1,5 @@
 package net.egorplaytv.caf.util;
 
-import net.egorplaytv.caf.CreateAndFood;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.FluidTags;
@@ -9,11 +8,6 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.IForgeRegistryEntry;
-
-import java.util.Collections;
 
 public class CAFTags {
 
@@ -24,9 +18,15 @@ public class CAFTags {
     }
 
     public static class Blocks {
-        private static void init(){}
+        private static void init(){ }
 
+        //Forge Tags
         public static final TagKey<Block> ALMOND = forgeTag("almond");
+        public static final TagKey<Block> NEEDS_COPPER_TOOL = forgeTag("needs_copper_tool");
+        public static final TagKey<Block> NEEDS_TANTALUM_TOOL = forgeTag("needs_tantalum_tool");
+        public static final TagKey<Block> NEEDS_TUNGSTEN_TOOL = forgeTag("needs_tungsten_tool");
+
+        //Create And Food Tags
         public static final TagKey<Block> ALMOND_LOGS = modTag("almond_logs");
         public static final TagKey<Block> FRAMED_WALLS = modTag("framed_walls");
         public static final TagKey<Block> BASALT_ORE_REPLACEABLE = modTag("basalt_ore_replaceable");
@@ -36,19 +36,15 @@ public class CAFTags {
         public static final TagKey<Block> WARPED_NYLIUM_ORE_REPLACEABLE = modTag("warped_nylium_ore_replaceable");
         public static final TagKey<Block> NETHERRACK_ORE_REPLACEABLE = modTag("netherrack_ore_replaceable");
         public static final TagKey<Block> BERRY_BUSHES = modTag("berry_bushes");
-
         public static final TagKey<Block> BARRELS = modTag("barrels");
         public static final TagKey<Block> CUTTING_BOARDS = modTag("cutting_boards");
-
         public static final TagKey<Block> MINEABLE_WITH_HAMMER = modTag("mineable/hammer");
-
-        public static final TagKey<Block> NEEDS_COPPER_TOOL = forgeTag("needs_copper_tool");
-        public static final TagKey<Block> NEEDS_TANTALUM_TOOL = forgeTag("needs_tantalum_tool");
-        public static final TagKey<Block> NEEDS_TUNGSTEN_TOOL = forgeTag("needs_tungsten_tool");
-
         public static final TagKey<Block> FAN_PROCESSING_CATALYSTS_FREEZING = modTag("fan_processing_catalysts/freezing");
 
+        //Minecraft Tags
 
+
+        // Utility
         public static TagKey<Block> modTag(String name) {
             return BlockTags.create(new ResourceLocation("caf", name));
         }
@@ -61,9 +57,9 @@ public class CAFTags {
     }
 
     public static class Items {
+        private static void init(){ }
 
-        private static void init(){}
-
+        //Forge Tags
         public static final TagKey<Item> STEEL = forgeTag("ingots/steel");
         public static final TagKey<Item> BRASS = forgeTag("ingots/brass");
         public static final TagKey<Item> ZINC = forgeTag("ingots/zinc");
@@ -84,47 +80,45 @@ public class CAFTags {
         public static final TagKey<Item> LUMIUM = forgeTag("ingots/lumium");
         public static final TagKey<Item> SIGNALUM = forgeTag("ingots/signalum");
         public static final TagKey<Item> INGOTS = forgeTag("ingots");
-
         public static final TagKey<Item> TORCHES = forgeTag("torches");
         public static final TagKey<Item> CHAINS = forgeTag("chains");
-
         public static final TagKey<Item> IRON_SHEET = forgeTag("plates/iron");
         public static final TagKey<Item> MEAT = forgeTag("meat");
+        public static final TagKey<Item> BANNER_PATTERNS = forgeTag("banner_patterns");
+        public static final TagKey<Item> MACARONI = forgeTag("macaroni");
+        public static final TagKey<Item> SMALL_BASE = forgeTag("small_base");
+        public static final TagKey<Item> COAL_DUST = forgeTag("dusts/coal");
+        public static final TagKey<Item> LANTERNS = forgeTag("lanterns");
+        public static final TagKey<Item> BOTTLES = forgeTag("bottles");
+        public static final TagKey<Item> CUT_TOOLS = forgeTag("cut_tools");
+        public static final TagKey<Item> BERRIES = forgeTag("berries");
 
+        //Create And Food Tags
         public static final TagKey<Item> RED_JUICE = modTag("grapes/red_juice");
         public static final TagKey<Item> WHITE_JUICE = modTag("grapes/white_juice");
         public static final TagKey<Item> GRAPES = modTag("grapes");
-
         public static final TagKey<Item> EGG = modTag("eggs/egg");
         public static final TagKey<Item> SPAWN_EGGS = modTag("eggs/spawn_eggs");
         public static final TagKey<Item> TURTLE_EGG = modTag("eggs/turtle_egg");
         public static final TagKey<Item> EGGS = modTag("eggs");
-        public static final TagKey<Item> BANNER_PATTERNS = forgeTag("banner_patterns");
-        public static final TagKey<Item> MACARONI = forgeTag("macaroni");
-        public static final TagKey<Item> SMALL_BASE = forgeTag("small_base");
         public static final TagKey<Item> ALMOND_LOGS = modTag("almond_logs");
-        public static final TagKey<Item> FENCE_GATES = tag("fence_gates");
-        public static final TagKey<Item> COAL_DUST = forgeTag("dusts/coal");
         public static final TagKey<Item> MOD_BERRIES = modTag("berries");
-        public static final TagKey<Item> BERRIES = forgeTag("berries");
-        public static final TagKey<Item> FUEL = modTag("fuel");
-        public static final TagKey<Item> CREATIVE_FUEL = modTag("creative_fuel");
-        public static final TagKey<Item> CUT_TOOLS = forgeTag("cut_tools");
-        public static final TagKey<Item> TERRACOTTA = tag("terracotta");
-        public static final TagKey<Item> DEAD_CORALS = tag("dead_corals");
-        public static final TagKey<Item> LANTERNS = forgeTag("lanterns");
-
-        public static final TagKey<Item> BOTTLES = forgeTag("bottles");
-
         public static final TagKey<Item> MARBLE = modTag("stone_types/marble");
         public static final TagKey<Item> MARBLE_BLACK_GALAXY = modTag("stone_types/marble_black_galaxy");
         public static final TagKey<Item> MARBLE_PERLIN_PINK = modTag("stone_types/marble_perlin_pink");
-
         public static final TagKey<Item> BARRELS = modTag("barrels");
         public static final TagKey<Item> CUTTING_BOARDS = modTag("cutting_boards");
         public static final TagKey<Item> TERRACES = modTag("terraces");
+        public static final TagKey<Item> FUEL = modTag("fuel");
+        public static final TagKey<Item> CREATIVE_FUEL = modTag("creative_fuel");
+
+        //Minecraft Tags
+        public static final TagKey<Item> FENCE_GATES = tag("fence_gates");
+        public static final TagKey<Item> TERRACOTTA = tag("terracotta");
+        public static final TagKey<Item> DEAD_CORALS = tag("dead_corals");
 
 
+        // Utility
         public static TagKey<Item> modTag(String name) {
             return ItemTags.create(new ResourceLocation("caf", name));
         }
@@ -137,9 +131,18 @@ public class CAFTags {
     }
 
     public static class Fluids {
-        private static void init(){}
+        private static void init(){ }
+
+        //Forge Tags
 
 
+        //Create And Food Tags
+
+
+        //Minecraft Tags
+
+
+        // Utility
         public static TagKey<Fluid> modTag(String name) {
             return FluidTags.create(new ResourceLocation("caf", name));
         }
